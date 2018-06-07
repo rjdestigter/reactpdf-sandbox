6c65ce73f3236acdef7dee0e90d2bb1c76a24312
Jacob33123 <jacobwatson33@gmail.com>
2018-06-07 11:35:44 -0600

    removing sample-manager and datatable components

--- packages/fc-table/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { Column, RendererFunc, RendererElementRecordProps, RendererElementProps, Renderer } from './src/types'
-
-export { default } from './components/Table'
--- packages/fc-table/src/components/Translate.ts
+++
@@ -1,8 +0,0 @@
-// Translateable Messages
-import messages from '../messages'
-
-// HOC
-import { translateHoc } from 'fc-intl'
-
-// Exports
-export default translateHoc({ messages })
